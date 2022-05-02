b392414664f4f358af7418ddde977c2a0651a2d3
Shkodran Gerguri <gerguris@gmail.com>
2022-05-02 16:50:58 +0100

    add first assignment solution

---
+++ 01-Fundamentals-Part-1/starter/assignment1.js
@@ -0,0 +1,16 @@
+let country = 'United Kingdom';
+let continent = 'Europe';
+let population = '70 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// logging the type of the data across the different variables
+let isIsland = true;
+let language;
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
